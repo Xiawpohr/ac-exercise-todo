3bbd22a9b7b70f0c22b08ea775cfd4efe37463b8
蕭博文 <xiaobowen@xiaobowende-MacBook-Pro.local>
2017-03-09 10:49:36 +0800

    generate the model and seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Todo.destroy_all
+todo_list = [
+	{task: 'photo album app', due_date: '2017-03-08'},
+	{task: 'todo list app', due_date: '2017-03-09'},
+	{task: 'database opration', due_date: '2017-03-10'}
+]
+todo_list.each { |todo| Todo.create( task: todo[:task], due_date: todo[:due_date]) }
